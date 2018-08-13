8436ca3dbeba9f750a7a2fa457242b1e05030d41
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-08-14 00:04:39 +0800

    运动模糊的处理

---
+++ Assets/MotionBlur/MotionBlur.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MotionBlur : PostEffectsBase
+{
+    public Shader motionBlurShader;
+    private Material motionBlurMaterial;
+
+    public Material Mat
+    {
+        get
+        {
+            if(motionBlurMaterial==null)
+            {
+                motionBlurMaterial = CheckShaderAndCreateMaterial(motionBlurShader, motionBlurMaterial);
+            }
+            return motionBlurMaterial;
+        }
+    }
+
+    [Range(0.0f, 0.9f)]
+    public float blurAmount = 0.5f;
+
+    private RenderTexture accumulationTexture;
+
+    private void OnDisable()
+    {
+        DestroyImmediate(accumulationTexture);
+    }
+
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if(Mat!=null)
+        {
+            if(accumulationTexture==null||accumulationTexture.width!=src.width
+                ||accumulationTexture.height!=src.height)
+            {
+                DestroyImmediate(accumulationTexture);
+                accumulationTexture = new RenderTexture(src.width, src.height, 0);
+                accumulationTexture.hideFlags = HideFlags.HideAndDontSave;
+                Graphics.Blit(src, accumulationTexture);
+            }
+
+            accumulationTexture.MarkRestoreExpected();
+
+            Mat.SetFloat("_BlurAmount", 1.0f - blurAmount);
+
+            Graphics.Blit(src, accumulationTexture, Mat);
+            Graphics.Blit(accumulationTexture, dest);
+        }
+        else
+        {
+            Graphics.Blit(src, dest);
+        }
+    }
+}
---
+++ Assets/MotionBlur/S_MotionBlur.shader
@@ -0,0 +1,76 @@
+﻿Shader "HCS/MotionBlur" 
+{
+	Properties 
+	{
+		_MainTex("Base (RGB)",2D)="white"{}
+		_BlurAmount("Blur Amount",float)=1.0		
+	}
+	SubShader 
+	{
+		CGINCLUDE
+
+		#include "UnityCG.cginc"
+
+		sampler2D _MainTex;
+		fixed _BlurAmount;
+
+		struct v2f
+		{
+			float4 pos:SV_POSITION;
+			half2 uv:TEXCOORD0;
+		};
+
+		v2f vert(appdata_img v)
+		{
+			v2f o;
+
+			o.pos=UnityObjectToClipPos(v.vertex);
+
+			o.uv=v.texcoord;
+
+			return o;
+		}
+		
+		fixed4 fragRGB(v2f i):SV_TARGET
+		{
+			return fixed4(tex2D(_MainTex,i.uv).rgb,_BlurAmount);
+		}
+
+		half4 fragA(v2f i):SV_TARGET
+		{
+			return tex2D(_MainTex,i.uv);
+		}
+
+		ENDCG
+
+		ZTest Always Cull off ZWrite off
+
+		pass
+		{
+			Blend SrcAlpha  OneMinusSrcAlpha
+			ColorMask RGB
+
+			CGPROGRAM
+
+			#pragma vertex vert
+			#pragma fragment fragRGB
+
+			ENDCG
+		}
+
+		pass
+		{
+			Blend One Zero
+			ColorMask A
+
+			CGPROGRAM
+
+			#pragma vertex vert
+			#pragma fragment fragA
+
+			ENDCG
+		}
+
+	}
+	Fallback off
+}
