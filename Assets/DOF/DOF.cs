70e059ac8e5c71cd8800d75a2f58d7392b77e0b0
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-08-31 11:58:50 +0800

    深度图的制作

---
+++ Assets/DOF/DOF.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DOF : PostEffectsBase
+{
+    public Shader replaceShader;
+
+    private Material mat;
+    public Material Mat
+    {
+        get
+        {
+            if(!mat)
+            {
+                mat = CheckShaderAndCreateMaterial(replaceShader, mat);
+            }
+            return mat;
+        }
+    }
+
+
+    private Camera cam;
+    public Camera Cam
+    {
+        get
+        {
+            if(!cam)
+            {
+                cam = GetComponent<Camera>();
+            }
+            return cam;
+        }
+    }
+
+
+    protected override void Start()
+    {
+        base.Start();
+        GetComponent<Camera>().depthTextureMode |= DepthTextureMode.Depth;
+    }
+
+    private void LateUpdate()
+    {
+        //if(Cam&&replaceShader)
+        //{
+        //    Cam.RenderWithShader(replaceShader, "RenderType");
+        //}
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if(Mat)
+        {
+            Graphics.Blit(src, dest, Mat);
+        }
+        else
+        {
+            Graphics.Blit(src, dest);
+        }
+    }
+}
---
+++ Assets/DOF/S_DepthTex.shader
@@ -0,0 +1,50 @@
+﻿Shader "HCS/DepthTex" 
+{
+	Properties 
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+	SubShader 
+	{
+		Tags{"RenderType"="Opaque"}
+
+		pass
+		{
+			CGPROGRAM
+
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			struct v2f
+			{
+				float2 uv:TEXCOORD0;
+				float4 pos:SV_POSITION;
+			};
+
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			sampler2D _CameraDepthTexture;
+
+			v2f vert(appdata_img v)
+			{
+				v2f o;
+				o.pos=UnityObjectToClipPos(v.vertex);
+				o.uv=TRANSFORM_TEX(v.texcoord,_MainTex); 
+				return o;
+			}
+
+			fixed4 frag(v2f i):SV_TARGET
+			{
+				float depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture,i.uv);
+				depth=Linear01Depth(depth);
+				return float4(depth,depth,depth,1);
+			}
+
+			ENDCG
+		}
+	}
+	FallBack off
+}
