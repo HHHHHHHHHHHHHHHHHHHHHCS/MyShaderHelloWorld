e46a054c1fa363a0b87c06b2ed6efb1f6b7c225b
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-09-13 21:49:51 +0800

    基础的面片生成完成  添加生成cube(未完成)

--- Assets/ProceduralGrid/ProceduralGrid.cs
+++ Assets/ProceduralGrid/ProceduralGrid.cs
@@ -17,41 +17,42 @@ public class ProceduralGrid : MonoBehaviour
 
     private IEnumerator Generate()
     {
-        WaitForSeconds wait = new WaitForSeconds(0.1f);
-
-        var mesh = GetComponent<MeshFilter>().mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
         mesh.name = "Procedural Grid";
 
         vertices = new Vector3[(xSize + 1) * (ySize + 1)];
-
+        Vector2[] uv = new Vector2[vertices.Length];
+        Vector4[] tangents = new Vector4[vertices.Length];
+        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
         for (int i = 0, y = 0; y <= ySize; y++)
         {
             for (int x = 0; x <= xSize; x++, i++)
             {
                 vertices[i] = new Vector3(x, y);
-                //yield return wait;
+                uv[i] = new Vector2((float)x / xSize, (float)y / ySize);
+                tangents[i] = tangent;
             }
         }
-
         mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.tangents = tangents;
 
-        int[] triangles = new int[(ySize + 1) * (xSize + 1) * 6];
+        int[] triangles = new int[xSize * ySize * 6];
 
-        for (int y = 0; y <= ySize; y++)
+        for (int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
         {
-            for (int x = 0; x <= xSize; x++)
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
             {
-                int pos = 6 * y * xSize + 6 * x;
-                int xpos = (xSize + 1) * y + x;
-                triangles[pos] = xpos;
-                triangles[pos + 3] = triangles[pos + 2] = xpos + 1;
-                triangles[pos + 4] = triangles[pos + 1] = xpos + xSize + 1;
-                triangles[pos + 5] = xpos + xSize + 2;
-                mesh.triangles = triangles;
-                yield return wait;
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+    
             }
         }
-
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+        yield break;
     }
 
     private void OnDrawGizmos()
---
+++ Assets/RoundedCube/RoundedCube.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class RoundedCube : MonoBehaviour
+{
+
+    public int xSize, ySize, zSize;
+
+    private Mesh mesh;
+    private Vector3[] vertices;
+
+    private void Awake()
+    {
+        StartCoroutine(Generate());
+    }
+
+    private IEnumerator Generate()
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Cube";
+        WaitForSeconds wait = new WaitForSeconds(0.05f);
+
+        yield return wait;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null)
+        {
+            return;
+        }
+        Gizmos.color = Color.black;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+}
