0384c3ceecfb860a4a565def6d21f0043fa5f484
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-09-14 12:00:15 +0800

    暂时先生成CUBE的顶点

--- Assets/RoundedCube/RoundedCube.cs
+++ Assets/RoundedCube/RoundedCube.cs
@@ -12,17 +12,65 @@ public class RoundedCube : MonoBehaviour
 
     private void Awake()
     {
-        StartCoroutine(Generate());
+        Generate();
     }
 
-    private IEnumerator Generate()
+    private void Generate()
     {
         GetComponent<MeshFilter>().mesh = mesh = new Mesh();
         mesh.name = "Procedural Cube";
-        WaitForSeconds wait = new WaitForSeconds(0.05f);
 
-        yield return wait;
+        CreateVertices();
     }
+
+    private void CreateVertices()
+    {
+        int cornerVertices = 8;
+        int edgeVertices = (xSize + ySize + zSize - 3) * 4;
+        int faceVertices = (
+            (xSize - 1) * (ySize - 1) +
+            (xSize - 1) * (zSize - 1) +
+            (ySize - 1) * (zSize - 1)) * 2;
+        vertices = new Vector3[cornerVertices + edgeVertices + faceVertices];
+
+        int v = 0;
+        for (int y = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                vertices[v++] = new Vector3(x, y, 0);
+            }
+            for (int z = 1; z <= zSize; z++)
+            {
+                vertices[v++] = new Vector3(xSize, y, z);
+            }
+            for (int x = xSize - 1; x >= 0; x--)
+            {
+                vertices[v++] = new Vector3(x, y, zSize);
+            }
+            for (int z = zSize - 1; z > 0; z--)
+            {
+                vertices[v++] = new Vector3(0, 0, z);
+            }
+        }
+        for (int z = 1; z < zSize; z++)
+        {
+            for (int x = 1; x < xSize; x++)
+            {
+                vertices[v++] = new Vector3(x, ySize, z);
+            }
+        }
+        for (int z = 1; z < zSize; z++)
+        {
+            for (int x = 1; x < xSize; x++)
+            {
+                vertices[v++] = new Vector3(x, 0, z);
+            }
+        }
+
+        mesh.vertices = vertices;
+    }
+
 
     private void OnDrawGizmos()
     {
