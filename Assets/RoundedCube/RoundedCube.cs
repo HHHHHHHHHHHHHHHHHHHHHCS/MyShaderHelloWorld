4432272e7618115bca4ddde8582a9398a02e630a
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-09-14 14:27:50 +0800

    创建了CUBE

--- Assets/RoundedCube/RoundedCube.cs
+++ Assets/RoundedCube/RoundedCube.cs
@@ -21,6 +21,7 @@ public class RoundedCube : MonoBehaviour
         mesh.name = "Procedural Cube";
 
         CreateVertices();
+        CreateTriangles();
     }
 
     private void CreateVertices()
@@ -50,7 +51,7 @@ public class RoundedCube : MonoBehaviour
             }
             for (int z = zSize - 1; z > 0; z--)
             {
-                vertices[v++] = new Vector3(0, 0, z);
+                vertices[v++] = new Vector3(0, y, z);
             }
         }
         for (int z = 1; z < zSize; z++)
@@ -71,6 +72,107 @@ public class RoundedCube : MonoBehaviour
         mesh.vertices = vertices;
     }
 
+    private int SetQuad(int[] triangles, int i, int v00, int v10, int v01, int v11)
+    {
+        triangles[i] = v00;
+        triangles[i + 1] = triangles[i + 4] = v01;
+        triangles[i + 2] = triangles[i + 3] = v10;
+        triangles[i + 5] = v11;
+        return i + 6;
+    }
+
+    private int CreateTopFace(int[] triangles, int t, int ring)
+    {
+        int v = ring * ySize;
+        for (int x = 0; x < xSize - 1; x++, v++)
+        {
+            t = SetQuad(triangles, t, v, v + 1, v + ring - 1, v + ring);
+        }
+        t = SetQuad(triangles, t, v, v + 1, v + ring - 1, v + 2);
+
+        int vMin = ring * (ySize + 1) - 1;
+        int vMid = vMin + 1;
+        int vMax = v + 2;
+        for (int z = 1; z < zSize - 1; z++, vMin--, vMid++, vMax++)
+        {
+            t = SetQuad(triangles, t, vMin, vMid, vMin - 1, vMid + xSize - 1);
+            for (int x = 1; x < xSize - 1; x++, vMid++)
+            {
+                t = SetQuad(
+                    triangles, t,
+                    vMid, vMid + 1, vMid + xSize - 1, vMid + xSize);
+            }
+            t = SetQuad(triangles, t, vMid, vMax, vMid + xSize - 1, vMax + 1);
+        }
+
+        int vTop = vMin - 2;
+        t = SetQuad(triangles, t, vMin, vMid, vTop + 1, vTop);
+        for (int x = 1; x < xSize - 1; x++, vTop--, vMid++)
+        {
+            t = SetQuad(triangles, t, vMid, vMid + 1, vTop, vTop - 1);
+        }
+        t = SetQuad(triangles, t, vMid, vTop - 2, vTop, vTop - 1);
+
+        return t;
+    }
+
+    private int CreateBottomFace(int[] triangles, int t, int ring)
+    {
+        int v = 1;
+        int vMid = vertices.Length - (xSize - 1) * (zSize - 1);
+        t = SetQuad(triangles, t, ring - 1, vMid, 0, 1);
+        for (int x = 1; x < xSize - 1; x++, v++, vMid++)
+        {
+            t = SetQuad(triangles, t, vMid, vMid + 1, v, v + 1);
+        }
+        t = SetQuad(triangles, t, vMid, v + 2, v, v + 1);
+
+        int vMin = ring - 2;
+        vMid -= xSize - 2;
+        int vMax = v + 2;
+
+        for (int z = 1; z < zSize - 1; z++, vMin--, vMid++, vMax++)
+        {
+            t = SetQuad(triangles, t, vMin, vMid + xSize - 1, vMin + 1, vMid);
+            for (int x = 1; x < xSize - 1; x++, vMid++)
+            {
+                t = SetQuad(
+                    triangles, t,
+                    vMid + xSize - 1, vMid + xSize, vMid, vMid + 1);
+            }
+            t = SetQuad(triangles, t, vMid + xSize - 1, vMax + 1, vMid, vMax);
+        }
+
+        int vTop = vMin - 1;
+        t = SetQuad(triangles, t, vTop + 1, vTop, vTop + 2, vMid);
+        for (int x = 1; x < xSize - 1; x++, vTop--, vMid++)
+        {
+            t = SetQuad(triangles, t, vTop, vTop - 1, vMid, vMid + 1);
+        }
+        t = SetQuad(triangles, t, vTop, vTop - 1, vMid, vTop - 2);
+
+        return t;
+    }
+
+    private void CreateTriangles()
+    {
+        int quads = (xSize * ySize + xSize * zSize + ySize * zSize) * 2;
+        int[] triangles = new int[quads * 6];
+        int ring = (xSize + zSize) * 2;
+        int t = 0, v = 0;
+
+        for (int y = 0; y < ySize; y++, v++)
+        {
+            for (int q = 0; q < ring - 1; q++, v++)
+            {
+                t = SetQuad(triangles, t, v, v + 1, v + ring, v + ring + 1);
+            }
+            t = SetQuad(triangles, t, v, v - ring + 1, v + ring, v + 1);
+        }
+        t = CreateTopFace(triangles, t, ring);
+        t = CreateBottomFace(triangles, t, ring);
+        mesh.triangles = triangles;
+    }
 
     private void OnDrawGizmos()
     {
