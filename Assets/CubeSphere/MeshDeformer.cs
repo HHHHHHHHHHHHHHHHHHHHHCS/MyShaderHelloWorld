67495defb5630d4639efdd23abe9c877495a3aa3
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-09-20 19:31:41 +0800

    小球被射线

---
+++ Assets/CubeSphere/MeshDeformer.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshDeformer : MonoBehaviour
+{
+    private Mesh deformingMesh;
+    /// <summary>
+    /// 原来的顶点   顶点的变化位置    顶点的速度
+    /// </summary>
+    private Vector3[] originalVertices, displacedVertices, vertexVelocities;
+
+    private void Start()
+    {
+        deformingMesh = GetComponent<MeshFilter>().mesh;
+        originalVertices = deformingMesh.vertices;
+        displacedVertices = new Vector3[originalVertices.Length];
+        for (int i = 0; i < originalVertices.Length; i++)
+        {
+            displacedVertices[i] = originalVertices[i];
+        }
+        vertexVelocities = new Vector3[originalVertices.Length];
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < displacedVertices.Length; i++)
+        {
+            UpdateVertex(i);
+        }
+        deformingMesh.vertices = displacedVertices;
+        deformingMesh.RecalculateNormals();
+    }
+
+    private void UpdateVertex(int i)
+    {
+        Vector3 velocity = vertexVelocities[i];
+        displacedVertices[i] += velocity * Time.deltaTime;
+    }
+
+    public void AddDeformingForce(Vector3 point, float force)
+    {
+        Debug.DrawLine(Camera.main.transform.position, point);
+        for (int i = 0; i < displacedVertices.Length; i++)
+        {
+            AddForceToVertex(i, point, force);
+        }
+    }
+
+    private void AddForceToVertex(int i, Vector3 point, float force)
+    {
+        Vector3 pointToVertex = displacedVertices[i] - point;
+        float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
+        float velocity = attenuatedForce * Time.deltaTime;
+        vertexVelocities[i] += pointToVertex.normalized * velocity;
+
+    }
+}
