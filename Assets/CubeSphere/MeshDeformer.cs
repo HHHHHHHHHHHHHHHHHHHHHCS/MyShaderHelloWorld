81ac04df7379c03bc7fdd6317e5f64c09e1bc6a3
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-09-22 10:30:46 +0800

    球的网络变形制作完毕

--- Assets/CubeSphere/MeshDeformer.cs
+++ Assets/CubeSphere/MeshDeformer.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter))]
 public class MeshDeformer : MonoBehaviour
 {
+    public float springForce = 20f;//回扯的力
+    public float damping = 5f;//力的回荡
+
     private Mesh deformingMesh;
     /// <summary>
     /// 原来的顶点   顶点的变化位置    顶点的速度
     /// </summary>
     private Vector3[] originalVertices, displacedVertices, vertexVelocities;
+    private float uniformScale = 1f;//球的缩放
+
 
     private void Start()
     {
+        uniformScale = transform.localScale.x;
         deformingMesh = GetComponent<MeshFilter>().mesh;
         originalVertices = deformingMesh.vertices;
         displacedVertices = new Vector3[originalVertices.Length];
@@ -36,11 +42,17 @@ public class MeshDeformer : MonoBehaviour
     private void UpdateVertex(int i)
     {
         Vector3 velocity = vertexVelocities[i];
-        displacedVertices[i] += velocity * Time.deltaTime;
+        Vector3 displacement = displacedVertices[i] - originalVertices[i];
+        displacement *= uniformScale;
+        velocity -= displacement * springForce * Time.deltaTime;
+        velocity *= 1f - damping * Time.deltaTime;
+        vertexVelocities[i] = velocity;
+        displacedVertices[i] += velocity * (Time.deltaTime / uniformScale);
     }
 
     public void AddDeformingForce(Vector3 point, float force)
     {
+        point = transform.InverseTransformPoint(point);
         Debug.DrawLine(Camera.main.transform.position, point);
         for (int i = 0; i < displacedVertices.Length; i++)
         {
@@ -51,6 +63,7 @@ public class MeshDeformer : MonoBehaviour
     private void AddForceToVertex(int i, Vector3 point, float force)
     {
         Vector3 pointToVertex = displacedVertices[i] - point;
+        pointToVertex *= uniformScale;
         float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
         float velocity = attenuatedForce * Time.deltaTime;
         vertexVelocities[i] += pointToVertex.normalized * velocity;
