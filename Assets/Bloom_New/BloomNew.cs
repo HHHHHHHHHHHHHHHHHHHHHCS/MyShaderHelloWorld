c5c93bee2857f41cc784815bfb325e84375040af
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-10-27 17:06:48 +0800

    bloom 未完成

---
+++ Assets/Bloom_New/BloomNew.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class BloomNew : MonoBehaviour
+{
+    [Range(1, 16)]
+    public int iterations = 1;
+    public Shader bloomShader;
+
+    [NonSerialized]
+    private Material bloom;
+
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if(!bloom)
+        {
+            bloom = new Material(bloomShader);
+            bloom.hideFlags = HideFlags.HideAndDontSave;
+        }
+
+
+        RenderTexture currentSource = Blur02(source, destination);
+
+
+
+
+
+        Graphics.Blit(currentSource, destination, bloom);
+        RenderTexture.ReleaseTemporary(currentSource);
+    }
+
+    private RenderTexture Blur00(RenderTexture source, RenderTexture destination)
+    {
+        int width = source.width;
+        int height = source.height;
+        RenderTextureFormat format = source.format;
+
+        RenderTexture currentDestination = RenderTexture.GetTemporary(
+            width, height, 0, format);
+
+        Graphics.Blit(source, currentDestination, bloom);
+        RenderTexture currentSource = currentDestination;
+
+        width /= 2 << iterations;
+        height /= 2 << iterations;
+
+        width = Mathf.Max(2, width);
+        height = Mathf.Max(2, height);
+
+        currentDestination = RenderTexture.GetTemporary(width, height, 0, format);
+        Graphics.Blit(currentSource, currentDestination, bloom);
+        RenderTexture.ReleaseTemporary(currentSource);
+        currentSource = currentDestination;
+        return currentSource;
+    }
+
+
+    private RenderTexture Blur01(RenderTexture source, RenderTexture destination)
+    {
+
+        int width = source.width / 2;
+        int height = source.height / 2;
+        RenderTextureFormat format = source.format;
+
+        RenderTexture currentDestination = RenderTexture.GetTemporary(
+            width, height, 0, format);
+
+        Graphics.Blit(source, currentDestination, bloom);
+        RenderTexture currentSource = currentDestination;
+
+
+        for (int i = 1; i < iterations; i++)
+        {
+            if (height < 2 || width < 2)
+            {
+                break;
+            }
+            width /= 2;
+            height /= 2;
+            currentDestination = RenderTexture.GetTemporary(width, height, 0, format);
+            Graphics.Blit(currentSource, currentDestination, bloom);
+            RenderTexture.ReleaseTemporary(currentSource);
+            currentSource = currentDestination;
+        }
+        return currentSource;
+    }
+
+    private RenderTexture Blur02(RenderTexture source, RenderTexture destination)
+    {
+        RenderTexture[] textures = new RenderTexture[16];
+        int width = source.width / 2;
+        int height = source.height / 2;
+        RenderTextureFormat format = source.format;
+
+        RenderTexture currentDestination = textures[0] = RenderTexture.GetTemporary(
+            width, height, 0, format);
+
+        Graphics.Blit(source, currentDestination, bloom);
+        RenderTexture currentSource = currentDestination;
+
+        int i = 1;
+        for (; i < iterations; i++)
+        {
+            if (height < 2 || width < 2)
+            {
+                break;
+            }
+            width /= 2;
+            height /= 2;
+            currentDestination = textures[i] = RenderTexture.GetTemporary(width, height, 0, format);
+            Graphics.Blit(currentSource, currentDestination, bloom);
+            //RenderTexture.ReleaseTemporary(currentSource);
+            currentSource = currentDestination;
+        }
+
+        for(i -=2;i>=0;i--)
+        {
+            currentDestination = textures[i];
+            textures[i] = null;
+            Graphics.Blit(currentSource, currentDestination);
+            RenderTexture.ReleaseTemporary(currentSource);
+            currentSource = currentDestination;
+        }
+
+        return currentSource;
+    }
+}
---
+++ Assets/Bloom_New/S_Bloom_New.shader
@@ -0,0 +1,54 @@
+﻿Shader "HCS/S_Bloom_New" 
+{
+	Properties 
+	{
+		_MainTex("Texture",2D)="white"{}
+	}
+
+	CGINCLUDE
+		#include "UnityCG.cginc"
+
+		sampler2D _MainTex;
+
+		struct a2v
+		{
+			float4 vertex :POSITION;
+			float2 uv :TEXCOORD0;
+		};
+
+		struct v2f
+		{
+			float4 pos:SV_POSITION;
+			float2 uv:TEXCOORD0;
+		};
+
+		v2f vert(a2v i)
+		{
+			v2f o;
+			o.pos = UnityObjectToClipPos(i.vertex);
+			o.uv = i.uv;
+			return o;
+		}
+
+	ENDCG	
+
+	SubShader 
+	{
+		Cull Off
+		ZTest Always
+		ZWrite Off
+
+		pass
+		{
+			CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment frag
+
+				half4 frag(v2f v):SV_TARGET
+				{
+					return tex2D(_MainTex,v.uv)*half4(1,0,0,0);
+				}
+			ENDCG
+		}
+	}
+}
