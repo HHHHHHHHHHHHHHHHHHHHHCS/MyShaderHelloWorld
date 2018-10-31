0ddaa6b11a1f0eb42bc4bc68ad80fc97116f1257
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-10-31 15:43:00 +0800

    景深模糊处理

---
+++ Assets/DepthOfField/DepthOfFieldEffect.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class DepthOfFieldEffect : MonoBehaviour
+{
+    [Range(0.1f, 100f)]
+    public float focusDistance = 10f;
+    [Range(0.1f, 10f)]
+    public float focusRange = 3f;
+
+    [HideInInspector]
+    public Shader dofShader;
+
+    [NonSerialized]
+    private Material dofMaterial;
+
+    private const int circleOfConfusionPass = 0;
+    private const int bokehPass = 1;
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (!dofMaterial)
+        {
+            dofMaterial = new Material(dofShader);
+            dofMaterial.hideFlags = HideFlags.HideAndDontSave;
+        }
+
+        dofMaterial.SetFloat("_FocusDistance", focusDistance);
+        dofMaterial.SetFloat("_FocusRange", focusRange);
+
+        RenderTexture coc = RenderTexture.GetTemporary(
+            src.width, src.height, 0, RenderTextureFormat.RHalf
+            , RenderTextureReadWrite.Linear);
+
+
+        Graphics.Blit(src, coc, dofMaterial, circleOfConfusionPass);
+
+        Graphics.Blit(src, dest, dofMaterial, bokehPass);
+
+
+        RenderTexture.ReleaseTemporary(coc);
+    }
+}
--- Assets/DepthOfField/S_DepthOfField.shader
+++ Assets/DepthOfField/S_DepthOfField.shader
@@ -8,8 +8,9 @@
 	CGINCLUDE
 		#include "UnityCG.cginc"
 
-		sampler2D _MainTex;
-		float4 _Maintex_TexelSize;
+		sampler2D _MainTex,_CameraDepthTexture;
+		float4 _MainTex_TexelSize;
+		float _FocusDistance,_FocusRange;
 
 		struct a2v
 		{
@@ -41,14 +42,48 @@
 		ZWrite off
 
 		pass
-		{
+		{// 0 circleOfConfusionPass
+			CGPROGRAM
+				#pragma vertex vert
+				#pragma fragment frag
+
+				half frag(v2f i):SV_TARGET
+				{
+					half depth = SAMPLE_DEPTH_TEXTURE(_CameraDepthTexture,i.uv);
+					depth=LinearEyeDepth(depth);
+
+					float coc =(depth-_FocusDistance)/_FocusRange;
+					coc = clamp(coc, -1, 1);
+					return coc;
+				}
+			ENDCG
+		}
+
+		pass
+		{// 1 bokenPass
 			CGPROGRAM
 				#pragma vertex vert
 				#pragma fragment frag
 
 				half4 frag(v2f i):SV_TARGET
 				{
-					return tex2D(_MainTex,i.uv);
+					half3 color = 0;
+					int weight = 0;
+					for(int u=-4;u<4;u++)
+					{
+						for(int v=-4;v<=4;v++)
+						{
+							float2 o = float2(u,v);
+							if(length(o)<=4)
+							{
+								o*=_MainTex_TexelSize.xy*2;
+								color +=tex2D(_MainTex ,i.uv+o).rgb;
+								weight+=1;
+							}
+						}
+					}
+					color *= 1.0/weight;
+					return half4(color,1);
 				}
 			ENDCG
 		}
