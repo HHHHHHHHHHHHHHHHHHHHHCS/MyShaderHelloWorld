f7d728b3b7ce0db4d8e5916d69ef2dd145370c2c
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-11-04 21:11:39 +0800

    边缘检测

--- Assets/FXAA/FXAAEffect.cs
+++ Assets/FXAA/FXAAEffect.cs
@@ -9,8 +9,22 @@ public class FXAAEffect : MonoBehaviour
     [HideInInspector]
     public Shader fxaaShader;
 
+    public enum LuminanceMode
+    {
+        Alpha,
+        Green,
+        Calculate
+    }
+
+    public LuminanceMode luminanceSource;
+
     [NonSerialized]
     private Material fxaaMaterial;
+
+
+    private const int  luminancePass = 0;
+    private const int fxaaPass = 1;
+
 
     private void OnRenderImage(RenderTexture src, RenderTexture dest)
     {
@@ -18,6 +32,29 @@ public class FXAAEffect : MonoBehaviour
         {
             fxaaMaterial = new Material(fxaaShader);
             fxaaMaterial.hideFlags = HideFlags.HideAndDontSave;
+        }
+
+        if(luminanceSource == LuminanceMode.Calculate)
+        {
+            fxaaMaterial.DisableKeyword("LUMINANCE_GREEN");
+            RenderTexture luminanceTex = RenderTexture.GetTemporary(
+                src.width, src.height, 0, src.format);
+
+            Graphics.Blit(src, luminanceTex, fxaaMaterial,luminancePass);
+            Graphics.Blit(luminanceTex, dest, fxaaMaterial, fxaaPass);
+            RenderTexture.ReleaseTemporary(luminanceTex);
+        }
+        else
+        {
+            if(luminanceSource==LuminanceMode.Green)
+            {
+                fxaaMaterial.EnableKeyword("LUMINANCE_GREEN");
+            }
+            else
+            {
+                fxaaMaterial.DisableKeyword("LUMINANCE_GREEN");
+            }
+            Graphics.Blit(src, dest, fxaaMaterial, fxaaPass);
         }
 
         Graphics.Blit(src, dest, fxaaMaterial);
