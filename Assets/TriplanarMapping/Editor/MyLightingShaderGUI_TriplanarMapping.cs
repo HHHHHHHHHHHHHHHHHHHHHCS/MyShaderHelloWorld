115f9d32d7ce01205f92c0262ddea512ffae6ecd
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2018-11-12 21:27:35 +0800

    shader GUI

--- Assets/TriplanarMapping/Editor/MyLightingShaderGUI_TriplanarMapping.cs
+++ Assets/TriplanarMapping/Editor/MyLightingShaderGUI_TriplanarMapping.cs
@@ -16,16 +16,30 @@ public class MyLightingShaderGUI_TriplanarMapping : MyLightingShaderGUI_Triplana
 
     void DoMaps()
     {
-
+        GUILayout.Label("Maps", EditorStyles.boldLabel);
+        editor.TexturePropertySingleLine(
+            MakeLabel("Albedo"), FindProperty("_MainTex"));
+        editor.TexturePropertySingleLine(
+            MakeLabel("MOHS", "Metallic (R) Occlusion (G) Height(B) Smoothness(A)")
+            , FindProperty("_MOHSMap"));
+        editor.TexturePropertySingleLine(
+            MakeLabel("Normals"),FindProperty("_MormalMap"));
     }
 
     void DoBlending()
     {
+        GUILayout.Label("Blending", EditorStyles.boldLabel);
 
+        editor.ShaderProperty(FindProperty("_BlendOffset"), MakeLabel("Offest"));
+        editor.ShaderProperty(FindProperty("_BlendExponent"), MakeLabel("Expoent"));
+        editor.ShaderProperty(FindProperty("_BlendHeightStrength"), MakeLabel("Height Strength"));
     }
 
     void DoOtherSettings()
     {
+        GUILayout.Label("Other Settings", EditorStyles.boldLabel);
 
+        editor.RenderQueueField();
+        editor.EnableInstancingField();
     }
 }
