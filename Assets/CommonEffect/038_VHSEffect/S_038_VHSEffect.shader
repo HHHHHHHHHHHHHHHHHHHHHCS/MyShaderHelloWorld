205dc2689e97a75045ab46bffe0db882b5ea189c
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-06-09 10:12:52 +0800

    038

---
+++ Assets/CommonEffect/038_VHSEffect/S_038_VHSEffect.shader
@@ -0,0 +1,67 @@
+﻿Shader "CommonEffect/S_038_VHSEffect"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" { }
+		_SecondaryTex ("Secondary Texture", 2D) = "white" { }
+		_OffsetNoiseX ("Offset Noise X", float) = 0.0
+		_OffsetNoiseY ("Offset Noise Y", float) = 0.0
+		_OffsetPosY ("Offset Position Y", float) = 0.0
+		_OffsetColor ("Offset Color", Range(0.005, 0.1)) = 0
+		_OffsetDistortion ("Offset Dustortion", float) = 500
+		_Insensity ("Mask Intensity", Range(0.0, 1)) = 1.0
+	}
+	SubShader
+	{
+		Pass
+		{
+			CGPROGRAM
+			
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+			
+			struct v2f
+			{
+				float4 pos: SV_POSITION;
+				float2 uv: TEXCOORD0;
+				float2 uv2: TEXCOORD1;
+			};
+			
+			half _OffsetNoiseX;
+			half _OffsetNoiseY;
+			
+			sampler2D _MainTex;
+			sampler2D _SecondaryTex;
+			
+			half _Intensity;
+			float _OffsetColor;
+			half _OffsetPosY;
+			half _OffsetDistortion;
+			
+			v2f vert(appdata_base v)
+			{
+				v2f o;
+				o.pos = UnityObjectToClipPos(v.vertex);
+				o.uv = v.texcoord;
+				o.uv2 = v.texcoord + float2(_OffsetNoiseX - 0.2f, _OffsetNoiseY);
+				return o;
+			}
+			
+			half4 frag(v2f i): SV_TARGET
+			{
+				i.uv = float2(frac(i.uv.x + cos((i.uv.y + _CosTime.y) * 100) / _OffsetDistortion), frac(i.uv.y + _OffsetPosY));
+				half4 col = tex2D(_MainTex, i.uv);
+				col.g = tex2D(_MainTex, i.uv + float2(_OffsetColor, _OffsetColor)).g;
+				col.b = tex2D(_MainTex, i.uv + float2(-_OffsetColor, -_OffsetColor)).b;
+				
+				half4 col2 = tex2D(_SecondaryTex, i.uv2);
+				
+				return lerp(col, col2, ceil(col2.r - _Intensity)) * (1 - ceil(saturate(abs(i.uv.y - 0.5) - 0.49)));
+			}
+			
+			ENDCG
+			
+		}
+	}
+}
---
+++ Assets/CommonEffect/038_VHSEffect/_038_VHSEffect.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class _038_VHSEffect : MonoBehaviour
+{
+    public Texture2D secondaryTex;
+
+    private Material mat;
+
+
+    private void Awake()
+    {
+        mat = new Material(Shader.Find("CommonEffect/S_038_VHSEffect"));
+        mat.SetTexture("_SecondaryTex", secondaryTex);
+        mat.SetFloat("_OffsetPosY", 0f);
+        mat.SetFloat("_OffsetColor", 0.01f);
+        mat.SetFloat("_OffsetDistortion", 480f);
+        mat.SetFloat("_Intensity", 0.64f);
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        mat.SetFloat("OffsetNoiseX", Random.Range(0f, 0.6f));
+        float offsetNoise = mat.GetFloat("_OffsetNoiseY");
+        mat.SetFloat("_OffsetNoiseY", offsetNoise + Random.Range(-0.03f, 0.03f));
+
+        float offsetPosY = mat.GetFloat("_OffsetPosY");
+        if (offsetPosY > 0.0f)
+        {
+            mat.SetFloat("_OffsetPosY", offsetPosY - Random.Range(0f, offsetPosY));
+        }
+        else if (offsetPosY < 0.0f)
+        {
+            mat.SetFloat("_OffsetPosY", offsetPosY + Random.Range(0f, -offsetPosY));
+        }
+        else if (Random.Range(0, 150) == 1)
+        {
+            mat.SetFloat("_OffsetPosY", Random.Range(-0.5f, 0.5f));
+        }
+
+        float offsetColor = mat.GetFloat("_OffsetColor");
+        if (offsetColor > 0.003f)
+        {
+            mat.SetFloat("_OffsetColor", offsetColor - 0.001f);
+        }
+        else if (Random.Range(0, 400) == 1)
+        {
+            mat.SetFloat("_OffsetColor", Random.Range(0.003f, 0.1f));
+        }
+
+        if (Random.Range(0, 15) == 1)
+        {
+            mat.SetFloat("_OffsetDistortion", Random.Range(1f, 480f));
+        }
+        else
+        {
+            mat.SetFloat("_OffsetDistortion", 480f);
+        }
+
+        Graphics.Blit(src, dest, mat);
+    }
+}
