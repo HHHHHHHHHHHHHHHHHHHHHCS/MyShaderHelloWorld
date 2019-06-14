b77b45d744a793c21cb3b0ea2aca37a14b9d812e
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-06-14 19:03:23 +0800

    kinoGlitch C# ready

--- Assets/ObjectEffect/KinoGlitch/KinoGlitchCamera.cs
+++ Assets/ObjectEffect/KinoGlitch/KinoGlitchCamera.cs
@@ -2,14 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
+[ExecuteInEditMode, RequireComponent(typeof(Camera))]
 public class KinoGlitchCamera : MonoBehaviour
 {
     public Shader shader;
 
     [Range(0, 1)] public float scanLineJitter = 0;
 
+    [Range(0, 1)] public float verticalJump = 0;
+
+    [Range(0, 1)] public float horizontalShake;
+
+    [Range(0, 1)] public float colorDrift = 0;
+
     private Material mat;
+    private float verticalJumpTime;
 
     private void OnEnable()
     {
@@ -17,7 +24,8 @@ public class KinoGlitchCamera : MonoBehaviour
         {
             return;
         }
-        mat = new Material(shader);
+
+        mat = new Material(shader) {hideFlags = HideFlags.HideAndDontSave};
     }
 
     private void OnRenderImage(RenderTexture src, RenderTexture dest)
@@ -27,6 +35,21 @@ public class KinoGlitchCamera : MonoBehaviour
             return;
         }
 
-        Graphics.Blit(src,dest,mat);
+        verticalJump += Time.deltaTime * verticalJump * 11.3f;
+
+        var sl_thresh = Mathf.Clamp01(1.0f - scanLineJitter * 1.2f);
+        var sl_disp = 0.002f + Mathf.Pow(scanLineJitter, 3) * 0.05f;
+        mat.SetVector("_ScanLineJitter", new Vector2(sl_disp, sl_thresh));
+
+        var vj = new Vector2(verticalJump, verticalJump);
+        mat.SetVector("_VerticalJump", vj);
+
+
+        mat.SetFloat("_HorizontalShake", horizontalShake * 0.2f);
+
+        var cd = new Vector2(colorDrift * 0.04f, Time.time * 606.11f);
+        mat.SetVector("_ColorDrift", cd);
+
+        Graphics.Blit(src, dest, mat);
     }
 }
