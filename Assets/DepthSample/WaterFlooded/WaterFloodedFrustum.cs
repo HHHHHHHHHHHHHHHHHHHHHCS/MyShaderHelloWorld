fc7279b6ffb02fe008e23a66e4226d14536c8597
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-09-16 10:23:05 +0800

    temp

---
+++ Assets/DepthSample/WaterFlooded/WaterFloodedFrustum.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterFloodedFrustum : MonoBehaviour
+{
+    public Material mat;
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        if (mat != null)
+        {
+            Camera cam = Camera.main;
+            float tanHalfFov = Mathf.Tan(0.5f * cam.fieldOfView * Mathf.Deg2Rad);
+            float halfHeight = tanHalfFov * cam.nearClipPlane;
+            float halfWidth = halfHeight * cam.aspect;
+
+            Vector3 toTop = cam.transform.up * halfHeight;
+            Vector3 toRight = cam.transform.right * halfWidth;
+            Vector3 toForward = cam.transform.forward * cam.nearClipPlane;
+
+            Vector3 toTopLeft = toForward + toTop - toRight;
+            Vector3 toBottomLeft = toForward - toTop - toRight;
+            Vector3 toTopRight = toForward + toTop + toRight;
+            Vector3 toBottomRight = toForward - toTop + toRight;
+
+
+            toTopLeft /= cam.nearClipPlane;
+            toBottomLeft /= cam.nearClipPlane;
+            toTopRight /= cam.nearClipPlane;
+            toBottomRight /= cam.nearClipPlane;
+
+            Matrix4x4 frustumDir = Matrix4x4.identity;
+            frustumDir.SetRow(0, toBottomLeft);
+            frustumDir.SetRow(1, toBottomRight);
+            frustumDir.SetRow(2, toTopLeft);
+            frustumDir.SetRow(3, toTopRight);
+            mat.SetMatrix("_FrustumDir", frustumDir);
+
+            Graphics.Blit(src, dest, mat);
+        }
+        else
+        {
+            Graphics.Blit(src, dest);
+        }
+    }
+}
