e2275e05afb2b105a7b1f51ad2d2385b6fcaf680
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-09-26 10:04:55 +0800

    add

---
+++ Assets/ScreenEffect/LensFlare/LensFlare_Bloom.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+[AddComponentMenu("Image Effects/Cinematic/Bloom")]
+[ImageEffectAllowedInSceneView]
+public class LensFlare_Bloom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
