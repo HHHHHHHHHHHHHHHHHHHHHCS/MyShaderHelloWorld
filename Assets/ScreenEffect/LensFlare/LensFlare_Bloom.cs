32c363f4548a7c714d2544ea7a0ce7467c661495
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2019-09-30 09:59:21 +0800

    temp

--- Assets/ScreenEffect/LensFlare/LensFlare_Bloom.cs
+++ Assets/ScreenEffect/LensFlare/LensFlare_Bloom.cs
@@ -28,31 +28,23 @@ public class LensFlare_Bloom : MonoBehaviour
             get => Mathf.Max(0.0f, Mathf.GammaToLinearSpace(threshold));
         }
 
-        [SerializeField,Range(0,1)]
-        [Tooltip("使低于/高于阈值之间的过渡逐渐进行")]
+        [SerializeField, Range(0, 1)] [Tooltip("使低于/高于阈值之间的过渡逐渐进行")]
         public float softKnee;
 
-        [SerializeField, Range(1, 7)]
-        [Tooltip("以与屏幕分辨率无关的方式更改遮罩效果的范围")]
+        [SerializeField, Range(1, 7)] [Tooltip("以与屏幕分辨率无关的方式更改遮罩效果的范围")]
         public float radius;
 
-        [SerializeField]
-        [Tooltip("混合图像的因子")]
-        public float intensity;
+        [SerializeField] [Tooltip("混合图像的因子")] public float intensity;
 
-        [SerializeField]
-        [Tooltip("高清,控制过滤器质量和缓冲区分辨率")]
+        [SerializeField] [Tooltip("高清,控制过滤器质量和缓冲区分辨率")]
         public bool highQuality;
 
-        [SerializeField]
-        [Tooltip("抗锯齿,额外的滤波器可以减少锯齿")]
+        [SerializeField] [Tooltip("抗锯齿,额外的滤波器可以减少锯齿")]
         public bool antiFlicker;
 
-        [Tooltip("增加屏幕的脏感觉")]
-        public Texture dirtTexture;
+        [Tooltip("增加屏幕的脏感觉")] public Texture dirtTexture;
 
-        [Min(0f), Tooltip("脏感觉强度")]
-        public float dirtIntensity;
+        [Min(0f), Tooltip("脏感觉强度")] public float dirtIntensity;
 
         public static Settings defaultSettings
         {
@@ -76,13 +68,11 @@ public class LensFlare_Bloom : MonoBehaviour
 
     #region Public Properties
 
-    [SerializeField]
-    public Settings settings = Settings.defaultSettings;
+    [SerializeField] public Settings settings = Settings.defaultSettings;
 
     #endregion
 
-    [SerializeField,HideInInspector]
-    private Shader shader;
+    [SerializeField, HideInInspector] private Shader shader;
 
     public Shader TheShader
     {
@@ -105,11 +95,102 @@ public class LensFlare_Bloom : MonoBehaviour
     {
         get
         {
-            //if (material == null)
-                //m_Material = ImageEffectHelper.CheckShaderAndCreateMaterial(shader);
-
+            if (material == null)
+                material = LensFlare_ImageEffectHelper.CheckShaderAndCreateMaterial(shader);
             return material;
         }
     }
 
+
+    #region Private Members
+
+    private const int maxIterations = 16;
+
+    private RenderTexture[] blurBuffer1 = new RenderTexture[maxIterations];
+    private RenderTexture[] blurBuffer2 = new RenderTexture[maxIterations];
+
+    private int id_threshold;
+    private int id_curve;
+    private int id_prefilterOffs;
+    private int id_sampleScale;
+    private int id_intensity;
+    private int id_dirtTex;
+    private int id_dirtIntensity;
+    private int id_baseTex;
+
+    private void Awake()
+    {
+        id_threshold = ToID("_Threshold");
+        id_curve = ToID("_Curve");
+        id_prefilterOffs = ToID("_PrefilterOffs");
+        id_sampleScale = ToID("_SampleScale");
+        id_intensity = ToID("_Intensity");
+        id_dirtTex = ToID("_DirtTex");
+        id_dirtIntensity = ToID("_DirtIntensity");
+        id_baseTex = ToID("_BaseTex");
+    }
+
+    private int ToID(string str)
+    {
+        return Shader.PropertyToID(str);
+    }
+
+    private void OnEnable()
+    {
+        if (!LensFlare_ImageEffectHelper.IsSupported(shader, true, false, this))
+            enabled = false;
+    }
+
+    private void OnDisable()
+    {
+        if (material != null)
+            DestroyImmediate(material);
+
+        material = null;
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        //手机不用HDR
+        var useRGBM = Application.isMobilePlatform;
+
+        var tw = src.width;
+        var th = src.height;
+
+        //半分辨率
+        if (!settings.highQuality)
+        {
+            tw /= 2;
+            th /= 2;
+        }
+
+        //blur buffer format
+        var rtFormat = useRGBM ? RenderTextureFormat.Default : RenderTextureFormat.DefaultHDR;
+
+        //1024=2^10 --> 10+radius-8
+        var logh = Mathf.Log(th, 2) + settings.radius - 8;
+        var logh_i = (int) logh;
+        var iterations = Mathf.Clamp(logh_i, 1, maxIterations);
+
+        //阀值
+        var threshold = settings.ThresholdLinear;
+        material.SetFloat(id_threshold, threshold);
+
+        //卷积属性
+        var knee = threshold * settings.softKnee + 1e-5f;
+        var curve = new Vector3(threshold - knee, knee * 2, 0.25f / knee);
+        material.SetVector(id_curve, curve);
+
+        //抗锯齿
+        var pfo = !settings.highQuality && settings.antiFlicker;
+        material.SetFloat(id_prefilterOffs, pfo ? -0.5f : 0.0f);
+
+        //强度
+        material.SetFloat(id_sampleScale, 0.5f + logh - logh_i);
+        material.SetFloat(id_intensity, Mathf.Max(0.0f, settings.intensity));
+
+        //TODO:
+    }
+
+    #endregion
 }
