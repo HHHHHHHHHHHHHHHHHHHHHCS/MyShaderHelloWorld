e34ba6e0b1027ae49f16c825f06505f85c943317
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-03-21 12:49:20 +0800

    add

--- Assets/Other/CombineCubemap/MaterialToPNG.cs
+++ Assets/Other/CombineCubemap/MaterialToPNG.cs
@@ -6,24 +6,26 @@ using UnityEngine;
 
 public class MaterialToPNG : MonoBehaviour
 {
-    public int size = 4096;
+    //如果是对付skybox 则宽:长=1:2
+    public int width = 4096, height = 2048;
     public string path = "Assets/Other/CombineCubemap/skybox.tga";
 
 
     private void Start()
     {
         var mat = GetComponent<MeshRenderer>().material;
-        RenderTexture rt = new RenderTexture(size, size, 0, RenderTextureFormat.ARGB32);
+        RenderTexture rt = new RenderTexture(width, height, 0, RenderTextureFormat.ARGB32);
         Graphics.Blit(null, rt, mat);
-        Texture2D t2d = new Texture2D(size, size, TextureFormat.RGB24, false, false);
+        Texture2D t2d = new Texture2D(width, height, TextureFormat.RGB24, false, false);
         RenderTexture.active = rt;
-        t2d.ReadPixels(new Rect(0, 0, size, size), 0, 0);
+        t2d.ReadPixels(new Rect(0, 0, width, height), 0, 0);
         t2d.Apply();
         using (var sw = File.Create(path))
         {
             var data = t2d.EncodeToTGA();
             sw.Write(data, 0, data.Length);
         }
+
         AssetDatabase.Refresh();
     }
 }
