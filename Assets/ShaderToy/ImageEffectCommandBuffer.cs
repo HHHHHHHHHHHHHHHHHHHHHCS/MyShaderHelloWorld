332002e7af666fdea53fb75c27a8bdd181f8e485
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-05-06 17:46:10 +0800

    add

--- Assets/ShaderToy/ImageEffectCommandBuffer.cs
+++ Assets/ShaderToy/ImageEffectCommandBuffer.cs
@@ -3,23 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
+[RequireComponent(typeof(Camera))]
 [ExecuteInEditMode]
 public class ImageEffectCommandBuffer : MonoBehaviour
 {
     public Shader effectShader;
 
-    private Material effectMaterial;
+    public Material effectMaterial;
 
     private CommandBuffer cb;
 
     private void Awake()
     {
-        if (!effectShader)
+        if (effectMaterial == null)
+        {
+            if (!effectShader)
+            {
+                return;
+            }
+
+            effectMaterial = new Material(effectShader)
+            {
+                hideFlags = HideFlags.HideAndDontSave
+            };
+        }
+
+        if (effectMaterial == null)
         {
             return;
         }
 
-        effectMaterial = new Material(effectShader);
 
         InitCommandBuffer();
 
@@ -29,17 +42,9 @@ public class ImageEffectCommandBuffer : MonoBehaviour
 
     private void InitCommandBuffer()
     {
-        cb = new CommandBuffer();
+        cb = new CommandBuffer {name = "AfterEverything"};
         cb.BeginSample("MyCommandBuffer");
         cb.Blit(BuiltinRenderTextureType.CameraTarget, BuiltinRenderTextureType.CameraTarget, effectMaterial);
         cb.EndSample("MyCommandBuffer");
-    }
-
-    private void OnDestroy()
-    {
-        if (effectMaterial)
-        {
-            GameObject.DestroyImmediate(effectMaterial);
-        }
     }
 }
