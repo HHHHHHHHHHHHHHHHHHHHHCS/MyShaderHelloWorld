8a069b99dbe6d0954b02d6b68aaa7723468b1740
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-06-03 11:32:19 +0800

    add

---
+++ Assets/ScreenEffect/TimeStop/S_TimeStop.shader
@@ -0,0 +1,94 @@
+﻿Shader "My/S_TimeStop"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" { }
+		_CenterX ("Center X", float) = 0.0
+		_CenterY ("Center Y", float) = 0.0
+		_Radius ("_Radius", float) = 0.2
+	}
+	SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+		
+		Pass
+		{
+			CGPROGRAM
+			
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+			
+			struct appdata
+			{
+				float4 vertex: POSITION;
+				float2 uv: TEXCOORD0;
+			};
+			
+			struct v2f
+			{
+				float2 uv: TEXCOORD0;
+				float4 vertex: SV_POSITION;
+			};
+			
+			sampler2D _MainTex;
+			float _CenterX, _CenterY;
+			float _Radius;
+			
+			v2f vert(appdata v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			float3 RGB2HSV(float3 c)
+			{
+				float4 K = float4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+				float4 p = lerp(float4(c.bg, K.wz), float4(c.gb, K.xy), step(c.b, c.g));
+				float4 q = lerp(float4(p.xyw, c.r), float4(c.r, p.yzx), step(p.x, c.r));
+				float d = q.x - min(q.w, q.y);
+				float e = 1.0e-10;
+				return float3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+			}
+			
+			float3 HSV2RGB(float3 c)
+			{
+				float4 K = float4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+				float3 p = abs(frac(c.xxx + K.xyz) * 6.0 - K.www);
+				return c.z * lerp(K.xxx, saturate(p - K.xxx), c.y);
+			}
+			
+			
+			float4 frag(v2f i): SV_Target
+			{
+				float aspect = _ScreenParams.x / _ScreenParams.y;
+				float x = (i.uv.x - 0.5f - _CenterX) * aspect;
+				float y = (i.uv.y - 0.5f - _CenterY) ;
+				float d = x * x + y * y;
+				
+				float4 col = tex2D(_MainTex, i.uv);
+				
+				
+				//hsv
+				float3 hsvColor = RGB2HSV(col.rgb);
+				//hsv 跟时间变化
+				hsvColor.x += lerp(0, 0.2, sin(UNITY_TWO_PI * frac(_Time.y * 0.5)));
+				hsvColor.x = frac(hsvColor.x);
+				
+				//反色
+				float3 reversedColor = 1 - HSV2RGB(hsvColor.rgb);
+				
+				if (d < _Radius * _Radius)
+					return float4(reversedColor, 1.0);
+				
+				return float4(col.rgb, 1.0);
+			}
+			ENDCG
+			
+		}
+	}
+}
--- Assets/ShaderToy/ImageEffectCommandBuffer.cs
+++ Assets/ShaderToy/ImageEffectCommandBuffer.cs
@@ -1,85 +1,92 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering;
 using UnityEngine.Rendering;
 
 [RequireComponent(typeof(Camera))]
 [ExecuteInEditMode]
 public class ImageEffectCommandBuffer : MonoBehaviour
 {
-    public bool inputMousePos;
-    public bool inputNoise;
+	public bool inputMousePos;
+	public bool inputNoise;
 
-    public Shader effectShader;
-    public Material effectMaterial;
-    
-    public Texture2D noise;
+	public Shader effectShader;
+	public Material effectMaterial;
 
-    private CommandBuffer cb;
+	public Texture2D noise;
 
+	private CommandBuffer cb;
 
-    private Camera mainCam;
+	private Camera mainCam;
 
-    private void Awake()
-    {
+	private void Awake()
+	{
+		if (effectMaterial == null)
+		{
+			if (!effectShader)
+			{
+				return;
+			}
 
-        if (effectMaterial == null)
-        {
-            if (!effectShader)
-            {
-                return;
-            }
+			effectMaterial = new Material(effectShader)
+			{
+				hideFlags = HideFlags.HideAndDontSave
+			};
+		}
 
-            effectMaterial = new Material(effectShader)
-            {
-                hideFlags = HideFlags.HideAndDontSave
-            };
-        }
-
-        if (effectMaterial == null)
-        {
-            return;
-        }
+		if (effectMaterial == null)
+		{
+			return;
+		}
 
 
-        InitCommandBuffer();
+		InitCommandBuffer();
 
-        mainCam = GetComponent<Camera>();
-        mainCam.AddCommandBuffer(CameraEvent.AfterEverything, cb);
+		mainCam = GetComponent<Camera>();
+		mainCam.AddCommandBuffer(CameraEvent.BeforeImageEffects, cb);
+
+		effectMaterial.SetVector("_MousePos", new Vector2(0.5f, 0.5f));
+		effectMaterial.SetTexture("_Noise", Texture2D.grayTexture);
+	}
+
+	private void InitCommandBuffer()
+	{
+		AudioSource ass;
+		cb = new CommandBuffer {name = "AfterEverything"};
+		cb.BeginSample("MyCommandBuffer");
+
+		int id = Shader.PropertyToID("CopyRT");
+		cb.GetTemporaryRT(id, Screen.width, Screen.height, 0, FilterMode.Bilinear);
+
+		//先把 CurrentActive 渲染出来 到id  不然是null 纯黑色
+		cb.Blit(BuiltinRenderTextureType.CurrentActive, id);
+		cb.Blit(id, BuiltinRenderTextureType.CameraTarget,effectMaterial);
+
+		cb.ReleaseTemporaryRT(id);
+		cb.EndSample("MyCommandBuffer");
+	}
 
 
-        effectMaterial.SetVector("_MousePos", new Vector2(0.5f,0.5f));
-        effectMaterial.SetTexture("_Noise", Texture2D.grayTexture);
-    }
+	private void Update()
+	{
+		if (inputMousePos && effectMaterial)
+		{
+			if (Input.GetMouseButton(0))
+			{
+				Vector2 mousePos = Input.mousePosition;
+				Vector2 viewPos = mainCam.ScreenToViewportPoint(mousePos);
+				viewPos.x = Mathf.Clamp01(viewPos.x);
+				viewPos.y = Mathf.Clamp01(viewPos.y);
+				//Debug.Log($"({viewPos.x:F5}, {viewPos.y:F5})");
+				effectMaterial.SetVector("_MousePos", viewPos);
+			}
+		}
 
-    private void InitCommandBuffer()
-    {
-        AudioSource ass;
-        cb = new CommandBuffer {name = "AfterEverything"};
-        cb.BeginSample("MyCommandBuffer");
-        cb.Blit(BuiltinRenderTextureType.CameraTarget, BuiltinRenderTextureType.CameraTarget, effectMaterial);
-        cb.EndSample("MyCommandBuffer");
-    }
-
-
-    private void Update()
-    {
-        if (inputMousePos && effectMaterial)
-        {
-            if (Input.GetMouseButton(0))
-            {
-                Vector2 mousePos = Input.mousePosition;
-                Vector2 viewPos = mainCam.ScreenToViewportPoint(mousePos);
-                viewPos.x = Mathf.Clamp01(viewPos.x);
-                viewPos.y = Mathf.Clamp01(viewPos.y);
-                //Debug.Log($"({viewPos.x:F5}, {viewPos.y:F5})");
-                effectMaterial.SetVector("_MousePos", viewPos);
-            }
-        }
-
-        if (inputNoise && effectMaterial && noise)
-        {
-            effectMaterial.SetTexture("_Noise", noise);
-        }
-    }
-} 
+		if (inputNoise && effectMaterial && noise)
+		{
+			effectMaterial.SetTexture("_Noise", noise);
+		}
+	}
+}
