0d20f54ed61681b2d97c9bf761687b81758a0606
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-07-20 09:54:50 +0800

    add

---
+++ Assets/ObjectEffect/RiverSystem/RiverSystem.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WayPoint
+{
+    public Vector3 pos;
+}
+
+[RequireComponent (typeof (MeshFilter))]
+[RequireComponent (typeof (MeshRenderer))]
+public class RiverSystem : MonoBehaviour
+{
+    public const string TAG = "River";
+    
+    //TODO:
+}
