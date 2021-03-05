6f3dfc51f85705493d9fc58b13cf41399937e489
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2021-03-05 19:50:32 +0800

    add

---
+++ Assets/Noise/VoronoiNoise/S_VoronoiNoise.shader
@@ -0,0 +1,112 @@
+﻿Shader "Noise/S_VoronoiNoise"
+{
+	Properties
+	{
+		_AngleOffset ("Angle Offset", Float) = 1
+		_CellDensity ("Cell Density", Float) = 1
+	}
+	SubShader
+	{
+		Tags { "RenderType" = "Opaque" }
+		LOD 100
+		
+		Pass
+		{
+			CGPROGRAM
+			
+			#pragma vertex vert
+			#pragma fragment frag
+			
+			#include "UnityCG.cginc"
+			
+			struct a2v
+			{
+				float4 vertex: POSITION;
+				float2 uv: TEXCOORD0;
+			};
+			
+			struct v2f
+			{
+				float2 uv: TEXCOORD0;
+				float4 vertex: SV_POSITION;
+			};
+			
+			float _AngleOffset;
+			float _CellDensity;
+			
+			inline float2 GradientNoiseDir(float2 p)
+			{
+				p = p % 289;
+				
+				float x = float(34 * p.x + 1) * p.x % 289 + p.y;
+				x = (34 * x + 1) * x % 289;
+				x = frac(x / 41) * 2 - 1;
+				return normalize(float2(x - floor(x + 0.5), abs(x) - 0.5));
+			}
+			
+			float GradientNoise(float2 uv, float scale)
+			{
+				float2 p = uv * scale;
+				float2 ip = floor(p);
+				float2 fp = frac(p);
+				float d00 = dot(GradientNoiseDir(ip), fp);
+				float d01 = dot(GradientNoiseDir(ip + float2(0, 1)), fp - float2(0, 1));
+				float d10 = dot(GradientNoiseDir(ip + float2(1, 0)), fp - float2(1, 0));
+				float d11 = dot(GradientNoiseDir(ip + float2(1, 1)), fp - float2(1, 1));
+				fp = fp * fp * fp * (fp * (fp * 6 - 15) + 10);
+				return lerp(lerp(d00, d01, fp.y), lerp(d10, d11, fp.y), fp.x) + 0.5;
+			}
+			
+			inline float2 VoronoiRandomVector(float2 uv, float offset)
+			{
+				float2x2 m = float2x2(15.27, 47.63, 99.41, 89.98);
+				uv = frac(sin(mul(uv, m)));
+				return float2(sin(uv.y * offset) * 0.5 + 0.5, cos(uv.x * offset) * 0.5 + 0.5);
+			}
+			
+			float2 Voronoi(float2 uv, float angleOffset, float cellDensity)
+			{
+				float2 g = floor(uv * cellDensity);
+				float2 f = frac(uv * cellDensity);
+				float t = 8.0;
+				float3 res = float3(8.0, 0.0, 0.0);
+				float2 ret = float2(0, 0);
+				
+				
+				for (int y = -1; y <= 1; y ++)
+				{
+					for (int x = -1; x <= 1; x ++)
+					{
+						float2 lattice = float2(x, y);
+						float2 offset = VoronoiRandomVector(lattice + g, angleOffset);
+						float d = distance(lattice + offset, f);
+						
+						if (d < res.x)
+						{
+							res = float3(d, offset.x, offset.y);
+							ret = res.xy;
+						}
+					}
+				}
+				
+				return ret;
+			}
+			
+			v2f vert(a2v v)
+			{
+				v2f o;
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = v.uv;
+				return o;
+			}
+			
+			half4 frag(v2f i): SV_Target
+			{
+				half2 noise = Voronoi(i.uv, _AngleOffset, _CellDensity);
+				return half4(noise.xy, 0, 1);
+			}
+			ENDCG
+			
+		}
+	}
+}
---
+++ Assets/Noise/WorleyNoise3D/WorleyNoise3D.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+using Debug = UnityEngine.Debug;
+using Random = System.Random;
+
+namespace Noise.WorleyNoise3D
+{
+	public class WorleyNoise3D : MonoBehaviour
+	{
+		private const int computeThreadGroupSize = 8;
+
+		private const int worley_Kernel = 0;
+		private const int normalize_Kernel = 1;
+
+		private static readonly int persistence_ID = Shader.PropertyToID("_Persistence");
+		private static readonly int resolution_ID = Shader.PropertyToID("_Resolution");
+		private static readonly int count_ID = Shader.PropertyToID("_Count");
+		private static readonly int threadGroup_ID = Shader.PropertyToID("_ThreadGroup");
+		private static readonly int minmax_ID = Shader.PropertyToID("_MinMax");
+		private static readonly int result_ID = Shader.PropertyToID("_Result");
+
+
+		private static readonly int pointsA_ID = Shader.PropertyToID("_PointsA");
+		private static readonly int pointsB_ID = Shader.PropertyToID("_PointsB");
+		private static readonly int pointsC_ID = Shader.PropertyToID("_PointsC");
+		private static readonly int numCellsA_ID = Shader.PropertyToID("_NumCellsA");
+		private static readonly int numCellsB_ID = Shader.PropertyToID("_NumCellsB");
+		private static readonly int numCellsC_ID = Shader.PropertyToID("_NumCellsC");
+		private static readonly int invertNoise_ID = Shader.PropertyToID("_InvertNoise");
+		private static readonly int tile_ID = Shader.PropertyToID("_Tile");
+
+		//--------------------
+		public ComputeShader noiseCompute;
+		public bool logComputeTime;
+
+		//--------------------
+		[Header("Noise Setting")] public int seed;
+		[Range(1, 50)] public int numDivisionsA = 5;
+		[Range(1, 50)] public int numDivisionsB = 10;
+		[Range(1, 50)] public int numDivisionsC = 15;
+
+		[Range(1, 128)] public int resolution = 64;
+		public float persistence = .5f;
+		public int tile = 1;
+		public bool invert = true;
+
+
+		//--------------------
+		//这里不用RenderTexture(Texture3D) 而是用 ComputeBuffer<float>
+		//想要实时的话 可以用RenderTexture(Texture3D) 
+		//private RenderTexture noise3DTexture;
+		private List<ComputeBuffer> buffersToRelease;
+		private int count;
+
+		private void Awake()
+		{
+			GeneratorNoise();
+		}
+
+		private void Init()
+		{
+			count = resolution * resolution * resolution;
+
+			//CreateTexture(ref noise3DTexture, resolution, "WorleyNoise3D");
+
+			if (buffersToRelease == null)
+			{
+				buffersToRelease = new List<ComputeBuffer>();
+			}
+			else
+			{
+				buffersToRelease.Clear();
+			}
+		}
+
+		private void ReleaseCB()
+		{
+			//release buffers
+			foreach (var buffer in buffersToRelease)
+			{
+				buffer.Release();
+			}
+
+			buffersToRelease.Clear();
+		}
+
+
+		public void GeneratorNoise()
+		{
+			Stopwatch timer = null;
+			if (logComputeTime)
+			{
+				timer = Stopwatch.StartNew();
+			}
+
+			Init();
+
+			ComputeBuffer resultCB = new ComputeBuffer(count, sizeof(float),
+				ComputeBufferType.Default);
+
+			//set values:
+			noiseCompute.SetFloat(persistence_ID, persistence);
+			noiseCompute.SetInt(resolution_ID, resolution);
+			noiseCompute.SetInt(count_ID, count);
+
+
+			//也可以用 noiseCompute.FindKernel("CSWorley");
+			//noiseCompute.SetTexture(worley_Kernel, result_ID, noise3DTexture);
+			noiseCompute.SetBuffer(worley_Kernel, result_ID, resultCB);
+			var minMaxBuffer = CreateBuffer(new int[] {int.MaxValue, 0}, sizeof(int),
+				minmax_ID, worley_Kernel);
+			UpdateWorley();
+
+			//dispatch noise gen kernel
+			//也可以用 noiseCompute.GetKernelThreadGroupSizes(0, out uint x, out uint y, out uint z);
+			int numThreadGroups = Mathf.CeilToInt(resolution / (float) computeThreadGroupSize);
+			noiseCompute.SetInt(threadGroup_ID, numThreadGroups);
+			noiseCompute.Dispatch(worley_Kernel, numThreadGroups, numThreadGroups, numThreadGroups);
+
+			// var results = new float[count];
+			// resultCB.GetData(results);
+			// int zz = 0;
+			// for (int i = 0; i < count; i++)
+			// {
+			// 	var res = results[i];
+			// 	Debug.Log(res);
+			// }
+
+			//set normalization  
+			noiseCompute.SetBuffer(normalize_Kernel, minmax_ID, minMaxBuffer);
+			noiseCompute.SetBuffer(normalize_Kernel, result_ID, resultCB);
+			//noiseCompute.SetTexture(normalize_Kernel, result_ID, noise3DTexture);
+			//dispatch normalization
+			noiseCompute.Dispatch(normalize_Kernel, numThreadGroups, numThreadGroups, numThreadGroups);
+
+
+#if UNITY_EDITOR
+			SaveAsset(resultCB);
+#endif
+
+
+			resultCB.Dispose();
+			ReleaseCB();
+
+			if (logComputeTime)
+			{
+				Debug.Log($"Noise Generation: {timer.ElapsedMilliseconds} ms");
+			}
+		}
+
+#if UNITY_EDITOR
+		private void SaveAsset(ComputeBuffer resultCB)
+		{
+			Texture3D asset = new Texture3D(resolution, resolution, resolution, TextureFormat.RFloat, false);
+			var results = new float[count];
+			var colors = new Color[count];
+
+			resultCB.GetData(results);
+			for (int i = 0; i < count; i++)
+			{
+				var res = results[i];
+				colors[i] = new Color(res, res, res, res);
+				// Debug.Log(res);
+			}
+
+			asset.SetPixels(colors);
+			UnityEditor.AssetDatabase.CreateAsset(asset, "Assets/Noise/WorleyNoise3D/" + "WorleyNoise3D" + ".asset");
+			UnityEditor.AssetDatabase.Refresh();
+		}
+#endif
+
+
+		private void CreateTexture(ref RenderTexture texture, int resolution, string textureName)
+		{
+			var format = GraphicsFormat.R16G16B16A16_UNorm;
+			if (texture == null || !texture.IsCreated() || texture.width != resolution ||
+			    texture.height != resolution || texture.volumeDepth != resolution || texture.graphicsFormat != format)
+			{
+				//Debug.Log ("Create tex: update noise: " + updateNoise);
+				if (texture != null)
+				{
+					texture.Release();
+				}
+
+				texture = new RenderTexture(resolution, resolution, 0, format, 0)
+				{
+					name = textureName,
+					volumeDepth = resolution,
+					enableRandomWrite = true,
+					dimension = TextureDimension.Tex3D,
+					wrapMode = TextureWrapMode.Repeat,
+					filterMode = FilterMode.Bilinear,
+				};
+				texture.Create();
+			}
+		}
+
+		private ComputeBuffer CreateBuffer(Array data, int stride, int bufferName, int kernel = 0)
+		{
+			var buffer = new ComputeBuffer(data.Length, stride, ComputeBufferType.Structured);
+			buffersToRelease.Add(buffer);
+			buffer.SetData(data);
+			noiseCompute.SetBuffer(kernel, bufferName, buffer);
+			return buffer;
+		}
+
+		private void CreateWorleyPointsBuffer(Random prng, int numCellsPerAxis, int bufferName)
+		{
+			var points = new Vector3[numCellsPerAxis * numCellsPerAxis * numCellsPerAxis];
+			float cellSize = 1f / numCellsPerAxis;
+
+			for (int x = 0; x < numCellsPerAxis; x++)
+			{
+				for (int y = 0; y < numCellsPerAxis; y++)
+				{
+					for (int z = 0; z < numCellsPerAxis; z++)
+					{
+						float randomX = (float) prng.NextDouble(); //return 0~1
+						float randomY = (float) prng.NextDouble();
+						float randomZ = (float) prng.NextDouble();
+						Vector3 randomOffset = new Vector3(randomX, randomY, randomZ) * cellSize;
+						//逐渐增长
+						Vector3 cellCorner = new Vector3(x, y, z) * cellSize;
+
+						int index = x + numCellsPerAxis * (y + z * numCellsPerAxis);
+						points[index] = cellCorner + randomOffset;
+					}
+				}
+			}
+
+			CreateBuffer(points, sizeof(float) * 3, bufferName, worley_Kernel);
+		}
+
+		private void UpdateWorley()
+		{
+			var prng = new Random(seed);
+			CreateWorleyPointsBuffer(prng, numDivisionsA, pointsA_ID);
+			CreateWorleyPointsBuffer(prng, numDivisionsB, pointsB_ID);
+			CreateWorleyPointsBuffer(prng, numDivisionsC, pointsC_ID);
+
+			noiseCompute.SetInt(numCellsA_ID, numDivisionsA);
+			noiseCompute.SetInt(numCellsB_ID, numDivisionsB);
+			noiseCompute.SetInt(numCellsC_ID, numDivisionsC);
+			noiseCompute.SetBool(invertNoise_ID, invert);
+			noiseCompute.SetInt(tile_ID, tile);
+		}
+	}
+}
